a007fa7edf554e17738481871f7c4cdd4ccae7d7
Nico <nicopuegher@gmail.com>
2023-05-25 18:33:24 -0300

    add chart navigation w/basic styles

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChartNav from '../ChartNav/ChartNav';
 import ChartBar from '../ChartBar/ChartBar';
 
 function Chart() {
   return (
     <section className="bg-gray-200">
+      <ChartNav />
       <ChartBar name="Jan" />
       <ChartBar name="Feb" />
       <ChartBar name="Mar" />
