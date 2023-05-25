a7816ee4214c3abfea1c8943256e0559a26c971b
Nico <nicopuegher@gmail.com>
2023-05-24 23:55:34 -0300

    add add component w/basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Name from './components/Name/Name';
+import Add from './components/Add/Add';
 
 function App() {
-  return <Name />;
+  return (
+    <>
+      <Name />
+      <Add />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Add/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Add() {
+  return (
+    <div className="flex justify-center bg-gray-200">
+      <button type="button" className="bg-gray-400">
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default Add;
