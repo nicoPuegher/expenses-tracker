93ef7542bcb91f4d97065efc37e7a0d57dde57a3
Nico <nicopuegher@gmail.com>
2023-05-28 16:29:56 -0300

    add months data object

---
+++ src/components/Chart/monthsData.js
@@ -0,0 +1,16 @@
+const monthsData = [
+  { id: 'mj1', name: 'Jan', expenses: 50 },
+  { id: 'mf2', name: 'Feb', expenses: 50 },
+  { id: 'mm3', name: 'Mar', expenses: 50 },
+  { id: 'ma4', name: 'Apr', expenses: 50 },
+  { id: 'mm5', name: 'May', expenses: 50 },
+  { id: 'mj6', name: 'Jun', expenses: 50 },
+  { id: 'mj7', name: 'Jul', expenses: 50 },
+  { id: 'ma8', name: 'Aug', expenses: 50 },
+  { id: 'ma9', name: 'Sep', expenses: 50 },
+  { id: 'mo10', name: 'Oct', expenses: 50 },
+  { id: 'mn11', name: 'Nov', expenses: 50 },
+  { id: 'md12', name: 'Dec', expenses: 50 },
+];
+
+export default monthsData;
