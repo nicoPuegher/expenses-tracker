30fbb7a56b1708206234a6a40788bdbfdac78ed9
Nico <nicopuegher@gmail.com>
2023-05-29 13:53:36 -0300

    add transaction date component

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomIcon from '../CustomIcon/CustomIcon';
+import TransactionDate from '../TransactionDate/TransactionDate';
 import Tag from '../Tag/Tag';
 
 const expenses = [
@@ -8,18 +8,21 @@ const expenses = [
     type: 'Food',
     name: 'Supermarket',
     amount: 35,
+    date: new Date(2023, 4, 29),
   },
   {
     id: 'tr1',
     type: 'Rent',
     name: 'Apartment',
     amount: 1500,
+    date: new Date(2023, 4, 29),
   },
   {
     id: 'tt1',
     type: 'Transport',
     name: 'Bus',
     amount: 2,
+    date: new Date(2023, 4, 26),
   },
 ];
 
@@ -27,9 +30,7 @@ function Transactions() {
   const transactions = expenses.map((transaction) => (
     <li key={transaction.id} className="flex py-2">
       <div className="flex w-full items-center gap-x-4">
-        <div className="flex h-7 w-7 flex-none items-center justify-center rounded bg-gray-300">
-          <CustomIcon iconType={transaction.type} />
-        </div>
+        <TransactionDate date={transaction.date} />
         <div className="flex min-w-0 flex-auto items-center justify-between">
           <div>
             <p className="text-sm font-semibold leading-6 text-gray-900">
