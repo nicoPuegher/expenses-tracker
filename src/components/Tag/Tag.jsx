2e1a89ff7fbaeb178ae3a03c26fedd0f19d63906
Nico <nicopuegher@gmail.com>
2023-05-29 12:09:05 -0300

    move icons inside tag

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CustomIcon from '../CustomIcon/CustomIcon';
 
 function Tag({ name, color }) {
-  const classes = `${color} inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10`;
-  return <span className={classes}>{name}</span>;
+  const classes = `${color} inline-flex items-center rounded px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 gap-x-1`;
+
+  return (
+    <span className={classes}>
+      <CustomIcon iconType={name} />
+      {name}
+    </span>
+  );
 }
 
 Tag.propTypes = {
