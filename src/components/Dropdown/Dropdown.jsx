dc0a8fa9894ddf6283e986c9bc9343967f1f39bb
Nico <nicopuegher@gmail.com>
2023-05-30 20:30:49 -0300

    refactor dropdown component classes

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -14,7 +14,7 @@ function Dropdown({ option, filterList }) {
         <button
           className={classNames(
             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-            'block px-4 py-2 text-sm'
+            'w-full block px-4 py-2 text-sm text-left'
           )}
           type="button"
         >
@@ -25,9 +25,9 @@ function Dropdown({ option, filterList }) {
   ));
 
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Button className="inline-flex gap-x-1.5 rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {option}
           <ChevronDownIcon
             className="-mr-1 h-5 w-5 text-gray-400"
@@ -45,7 +45,7 @@ function Dropdown({ option, filterList }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
           <div className="py-1">{items}</div>
         </Menu.Items>
       </Transition>
