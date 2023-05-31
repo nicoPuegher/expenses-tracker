c21a7a0961c16ce862bf7a134d25a99c1e295d6a
Nico <nicopuegher@gmail.com>
2023-05-31 15:09:20 -0300

    add rounded corners to add component

--- src/components/Add/Add.jsx
+++ src/components/Add/Add.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Add() {
   return (
-    <section className="flex justify-center bg-gray-50 py-2">
+    <section className="flex justify-center rounded bg-gray-50 py-2">
       <button className="rounded bg-gray-300 px-6 py-2" type="button">
         Add
       </button>
