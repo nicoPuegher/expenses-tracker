c44847634b46c9845d992796bf2e64d2a896cf4a
Nico <nicopuegher@gmail.com>
2023-06-02 12:48:33 -0300

    make y-padding on app smaller

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Transactions from './components/Transactions/Transactions';
 
 function App() {
   return (
-    <div className="flex h-full flex-col px-2 py-4">
+    <div className="flex h-full flex-col px-2 py-3">
       <Add />
       <Filter title="Filter by" option="Year" />
       <Chart />
