028e16b3c2956f8ce16870c4dd06f29f9b77db9d
Nico <nicopuegher@gmail.com>
2023-06-08 17:05:18 -0300

    add new component

---
+++ src/components/New/New.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Modal from '../Modal/Modal';
+import Add from '../Add/Add';
+
+function New() {
+  return (
+    <>
+      {/* <Modal /> */}
+      <Add />
+    </>
+  );
+}
+
+export default New;
