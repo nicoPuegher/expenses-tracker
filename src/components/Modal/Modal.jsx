040bb27499553281ca227f6eefc9d7a469b0b1ae
Nico <nicopuegher@gmail.com>
2023-06-09 15:22:07 -0300

    add material UI components and add title input in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Dialog, Transition } from '@headlessui/react';
+import TextField from '@mui/material/TextField';
 
 function Modal({ isOpen, onCloseModal }) {
   const cancelButtonRef = useRef(null);
@@ -38,43 +39,47 @@ function Modal({ isOpen, onCloseModal }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative overflow-hidden rounded bg-white px-2 py-3 shadow-xl transition-all">
-                <div className="bg-white">
-                  <div>
-                    <div className="text-center">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        New Expense
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
-                        </p>
+              <Dialog.Panel className="relative w-full overflow-hidden rounded bg-white px-2 py-3 shadow-xl transition-all">
+                <form>
+                  <div className="bg-white">
+                    <div>
+                      <div className="text-center">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          New Expense
+                        </Dialog.Title>
+                        <div>
+                          <TextField
+                            id="outlined-basic"
+                            label="Title"
+                            variant="outlined"
+                            size="small"
+                            fullWidth
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="bg-gray-50">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500"
-                    onClick={() => onCloseModal()}
-                  >
-                    Add
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    onClick={() => onCloseModal()}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
-                </div>
+                  <div className="bg-gray-50">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500"
+                      onClick={() => onCloseModal()}
+                    >
+                      Add
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      onClick={() => onCloseModal()}
+                      ref={cancelButtonRef}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
               </Dialog.Panel>
             </Transition.Child>
           </div>
