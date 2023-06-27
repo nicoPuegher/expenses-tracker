732fa7ef725f3c57f882d5347960b7361516379f
Nico <nicopuegher@gmail.com>
2023-06-27 14:09:12 -0300

    remove margin from header h1

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Name() {
   return (
-    <h1 className="mb-4 hidden text-center text-xl font-semibold">
+    <h1 className="hidden text-center text-xl font-semibold">
       Expenses Tracker
     </h1>
   );
