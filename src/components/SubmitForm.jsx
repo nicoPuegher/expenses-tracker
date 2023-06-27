bae3febcdcb9b1e25bdf582f9e0c154556e718af
Nico <nicopuegher@gmail.com>
2023-06-27 16:44:52 -0300

    refactoring, separate form buttons to its own component

--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -7,6 +7,8 @@ import PropTypes from 'prop-types';
 import BasicDatePicker from './BasicDatePicker';
 import types from '../utils/types-list';
 
+import FormButtons from './FormButtons';
+
 function SubmitForm({ onCloseModal }) {
   // const [formValues, setFormValues] = useState({
   //   title: {
@@ -87,21 +89,7 @@ function SubmitForm({ onCloseModal }) {
           </div>
         </div>
       </div>
-      <div className="bg-gray-50">
-        <button
-          type="submit"
-          className="inline-flex w-full justify-center rounded bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500"
-        >
-          Add
-        </button>
-        <button
-          type="button"
-          className="mt-3 inline-flex w-full justify-center rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          onClick={() => onCloseModal()}
-        >
-          Cancel
-        </button>
-      </div>
+      <FormButtons onCloseModal={() => onCloseModal()} />
     </form>
   );
 }
