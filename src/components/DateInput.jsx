16341a0ec68c14645dbf055937956e0abb2f8daf
Nico <nicopuegher@gmail.com>
2023-06-29 19:46:57 -0300

    refactoring, add min and max values to select date input

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -3,18 +3,26 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
 function DateInput({ onChange, error, helperText }) {
+  const min = dayjs(new Date(2021, 0, 1));
+  const max = dayjs(new Date(2023, 11, 31));
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
         label="Pick a date"
+        minDate={min}
+        maxDate={max}
+        views={['year', 'month', 'day']}
         slotProps={{
           textField: {
             size: 'small',
             onBlur: (e) => onChange({ name: 'date', value: e.target.value }),
             error,
             helperText,
+            minDate: min,
             fullWidth: true,
             required: true,
           },
