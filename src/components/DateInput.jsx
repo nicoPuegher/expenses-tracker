8663d55104908d744ac7be2b80feae37069e4d2e
Nico <nicopuegher@gmail.com>
2023-07-03 16:01:56 -0300

    refactoring, separate date validation from component

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -4,27 +4,13 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import PropTypes from 'prop-types';
 import dayjs from 'dayjs';
+import checkDate from '../utils/submission-validation/check-date';
 
 function DateInput({ onChange, error, helperText }) {
   const min = dayjs(new Date(2021, 0, 1));
   const max = dayjs(new Date(2023, 11, 31));
 
-  const checkDate = (date) => {
-    const isValid = dayjs(date).isValid();
-    if (!isValid) {
-      onChange({ name: 'date', value: '' });
-      return;
-    }
-
-    const selectedYear = dayjs(date).year();
-    if (selectedYear < 2021 || selectedYear > 2023) {
-      onChange({ name: 'date', value: '' });
-      return;
-    }
-
-    const { $M: month, $D: day, $y: year } = dayjs(date);
-    onChange({ name: 'date', value: `${month + 1}-${day}-${year}` });
-  };
+  const validateHandler = (date) => onChange(checkDate(date));
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -36,14 +22,14 @@ function DateInput({ onChange, error, helperText }) {
         slotProps={{
           textField: {
             size: 'small',
-            onBlur: (e) => checkDate(e.target.value),
+            onBlur: (e) => validateHandler(e.target.value),
             error,
             helperText,
             fullWidth: true,
             required: true,
           },
         }}
-        onChange={(e) => checkDate(e ? e.$d : '')}
+        onChange={(e) => validateHandler(e ? e.$d : '')}
       />
     </LocalizationProvider>
   );
