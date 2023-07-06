012517d7891fa5a1082c4bd539ae0c6b07c45819
Nico <nicopuegher@gmail.com>
2023-07-06 17:27:17 -0300

    update expenses prop names for displaying expenses

--- src/components/Main/Expenses/Expenses.jsx
+++ src/components/Main/Expenses/Expenses.jsx
@@ -20,11 +20,13 @@ function Expenses({ currentFilter }) {
           <div className="flex flex-auto items-center justify-between">
             <div>
               <p className="pb-1.5 text-sm font-semibold leading-none text-gray-900">
-                {singleExpense.name}
+                {singleExpense.title}
               </p>
               <Tag name={singleExpense.type} color="bg-gray-50" />
             </div>
-            <p className="text-base text-gray-500">${singleExpense.amount}</p>
+            <p className="text-base text-gray-500">
+              ${singleExpense.numAmount}
+            </p>
           </div>
         </div>
       </li>
