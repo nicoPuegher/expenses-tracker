81c7932ba0fd78942279a878eddc857101065cc1
Nico <nicopuegher@gmail.com>
2023-07-06 17:23:12 -0300

    update expense date component props display

--- src/components/Main/Expenses/ExpenseDate.jsx
+++ src/components/Main/Expenses/ExpenseDate.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 
 function ExpenseDate({ date }) {
-  const month = date.toLocaleString('en-US', { month: 'short' });
-  const day = date.toLocaleString('en-US', { day: '2-digit' });
-  const year = date.getFullYear();
+  const { $d: expenseDate } = dayjs(date);
+  const month = expenseDate.toLocaleString('en-US', { month: 'short' });
+  const day = expenseDate.toLocaleString('en-US', { day: '2-digit' });
+  const year = expenseDate.getFullYear();
 
   return (
     <div className="flex flex-col items-center justify-center rounded bg-gray-400 px-2 py-0.5 text-xs">
@@ -16,7 +18,7 @@ function ExpenseDate({ date }) {
 }
 
 ExpenseDate.propTypes = {
-  date: PropTypes.instanceOf(Date).isRequired,
+  date: PropTypes.string.isRequired,
 };
 
 export default ExpenseDate;
