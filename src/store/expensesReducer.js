6b66e643d10f9db62825dc4dc95c0ae9fb0e1dcf
Nico <nicopuegher@gmail.com>
2023-07-06 16:34:08 -0300

    add logic for reducer action type add

--- src/store/expensesReducer.js
+++ src/store/expensesReducer.js
@@ -1,13 +1,23 @@
 import dayjs from 'dayjs';
+import { uniqueId } from 'lodash';
 import defaultState from './expenses-default';
 
 const expensesReducer = (state, action) => {
   if (action.type === 'ADD') {
-    // console.log(action);
     const { title, amount, date, type } = action.expense;
-    // console.log(Number(amount).toFixed(2));
+    const numAmount = Number(amount).toFixed(2);
+
     const year = dayjs(date).year();
-    console.log(state.expenses[year]);
+    const month = dayjs(date).month();
+    const arr = state.expenses[year][month];
+
+    arr.push({
+      id: uniqueId(`${year}-${month}_`),
+      title,
+      numAmount,
+      date,
+      type,
+    });
   }
 
   return defaultState;
