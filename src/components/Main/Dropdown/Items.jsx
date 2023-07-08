c728ee3c223bf731fc1a1b74f0bee6981a11311c
Nico <nicopuegher@gmail.com>
2023-07-08 12:34:30 -0300

    refactoring, move class name function and menu items from dropdown to a separate file

---
+++ src/components/Main/Dropdown/Items.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Menu } from '@headlessui/react';
+import PropTypes from 'prop-types';
+import classNames from '../../../utils/dropdown/class-names';
+
+function Items({ filterList, clickHandler }) {
+  return filterList.map((item) => (
+    <Menu.Item key={item.id}>
+      {({ active }) => (
+        <button
+          className={classNames(
+            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+            'w-full block px-4 py-2 text-sm text-left'
+          )}
+          type="button"
+          value={item.value}
+          onClick={clickHandler}
+        >
+          {item.value}
+        </button>
+      )}
+    </Menu.Item>
+  ));
+}
+
+Items.propTypes = {
+  filterList: PropTypes.instanceOf(Object).isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
+
+export default Items;
