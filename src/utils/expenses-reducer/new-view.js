3a5f78d3081c3ec38a11ad97899608adfa917290
Nico <nicopuegher@gmail.com>
2023-07-11 15:21:56 -0300

    fix reducer new view state on submit expense to use updated context

--- src/utils/expenses-reducer/new-view.js
+++ src/utils/expenses-reducer/new-view.js
@@ -1,8 +1,28 @@
-const newView = ({ currentView: { filter } }, { month }) => {
-  const newCurrentView = filter
-    ? { filter: true, month: false, current: null }
-    : { filter: false, month: true, current: month };
+const newView = ({ currentView: { filter }, month: { currentMonth } }) => {
+  const filterView = {
+    ...filter,
+    month: {
+      active: false,
+      current: null,
+      name: null,
+    },
+  };
+
+  const monthView = {
+    filter: {
+      ...filter,
+      active: false,
+    },
+    month: {
+      active: true,
+      current: currentMonth,
+      name: '',
+    },
+  };
+
+  const newCurrentView = filter.active ? filterView : monthView;
 
   return newCurrentView;
 };
+
 export default newView;
