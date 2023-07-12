17de262c1d9b37bd1c586b172f59db2c3435dd96
Nico <nicopuegher@gmail.com>
2023-07-12 20:40:38 -0300

    on click single expense make options appear on top with same styles

--- src/components/Main/Expenses/ExpenseBtn/ExpenseBtn.jsx
+++ src/components/Main/Expenses/ExpenseBtn/ExpenseBtn.jsx
@@ -21,7 +21,7 @@ function ExpenseBtn({ singleExpense }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 top-0 z-10 mt-2 w-44 origin-top-right rounded bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+        <Menu.Items className="absolute right-0 top-0 z-10 h-full w-full rounded bg-white px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           <div className="w-full py-1">
             <Menu.Item>
               {({ active }) => (
