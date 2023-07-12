93c3ca6aac15f6d3dd4de431e23ad4e1b8a01523
Nico <nicopuegher@gmail.com>
2023-07-12 16:20:16 -0300

    add healthcare icon expense

--- src/components/Main/Expenses/CustomIcon.jsx
+++ src/components/Main/Expenses/CustomIcon.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+
 import HomeIcon from '@mui/icons-material/Home';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
@@ -12,6 +14,8 @@ function CustomIcon({ iconType }) {
       return <AccountBalanceIcon fontSize="inherit" />;
     case 'Food':
       return <LocalDiningIcon fontSize="inherit" />;
+    case 'Healthcare':
+      return <LocalHospitalIcon fontSize="inherit" />;
     case 'Rent':
       return <HomeIcon fontSize="inherit" />;
     case 'Transport':
