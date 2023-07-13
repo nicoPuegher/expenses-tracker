2046be0d107146475d182b668ce5b2bb00987721
Nico <nicopuegher@gmail.com>
2023-07-13 15:51:32 -0300

    in delete expense, get hash with expense year

--- src/utils/expenses-reducer/delete-expense.js
+++ src/utils/expenses-reducer/delete-expense.js
@@ -1,7 +1,9 @@
 import formatDate from '../format-helpers/format-date';
+import yearHash from '../hash/year-hash';
 
 const deleteExpense = (state, expense) => {
   const { month, year } = formatDate(expense);
+  const hash = yearHash(year);
 
   return {
     ...state,
