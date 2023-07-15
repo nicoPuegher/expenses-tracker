c932bcffceedb25ecd352920f2dac9afdf5d6185
Nico <nicopuegher@gmail.com>
2023-07-14 21:04:21 -0300

    refactoring, use lodash to simplify yearly filter helper function

--- src/utils/expenses-reducer/filter/yearly-filter.js
+++ src/utils/expenses-reducer/filter/yearly-filter.js
@@ -1,16 +1,21 @@
-const yearlyFilter = (state, value) => ({
-  ...state,
-  currentView: {
-    filter: {
-      active: true,
-      current: value,
-    },
-    month: {
-      active: false,
-      current: null,
-      name: null,
-    },
-  },
-});
+import { cloneDeep } from 'lodash';
+
+const yearlyFilter = (state, value) => {
+  const updatedState = cloneDeep(state);
+  const { currentView } = updatedState;
+
+  currentView.filter = {
+    active: true,
+    current: value,
+  };
+
+  currentView.month = {
+    active: false,
+    current: null,
+    name: null,
+  };
+
+  return updatedState;
+};
 
 export default yearlyFilter;
