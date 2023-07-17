6b4c86be5fdb7f8f9a3c78eb0b906b8665c86234
Nico <nicopuegher@gmail.com>
2023-07-17 20:22:17 -0300

    create a styled text field with custom css for error mode

---
+++ src/components/Inputs/StyledTextField.jsx
@@ -0,0 +1,14 @@
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+
+const StyledTextField = styled(TextField)({
+  '& label': { color: '#6b7280' }, // gray-500
+  '& label.Mui-error': { color: '#f87171' }, // red-400
+  '& label span.Mui-error': { color: '#f87171' }, // red-400
+  '& p.Mui-error': { color: '#f87171' }, // red-400
+  '& .MuiOutlinedInput-root.Mui-error': {
+    '& fieldset': { 'border-color': '#f87171' }, // red-400
+  },
+});
+
+export default StyledTextField;
