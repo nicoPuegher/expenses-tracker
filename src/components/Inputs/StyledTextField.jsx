5bcc0a2bbd86b1a18069ba20559493967f3a6524
Nico <nicopuegher@gmail.com>
2023-07-17 20:52:55 -0300

    add hover style and better format styled text field

--- src/components/Inputs/StyledTextField.jsx
+++ src/components/Inputs/StyledTextField.jsx
@@ -1,36 +1,45 @@
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 
-const gray = '#6b7280'; // gray-500
-const slate = '#334155'; // slate-700
-const red = '#f87171'; // red-400
-const indigo = '#818cf8'; // indigo-400
+const slate700 = '#334155';
+const slate400 = '#94a3b8';
+const red400 = '#f87171';
+const indigo400 = '#818cf8';
 
 const StyledTextField = styled(TextField)({
   // Default
-  '& label': { color: gray },
-  '& div input': { color: slate },
+  '& label': { color: slate400 },
+  '& label span': { color: slate400 },
+  '& div input': { color: slate700 },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': { 'border-color': slate400 },
+  },
+
+  // Hover
+  '&:hover .MuiOutlinedInput-root': {
+    '& fieldset': { 'border-color': slate700 },
+  },
 
   // Error
-  '& label.Mui-error': { color: red },
-  '& label span.Mui-error': { color: red },
-  '& p.Mui-error': { color: red },
+  '& label.Mui-error': { color: red400 },
+  '& label span.Mui-error': { color: red400 },
+  '& p.Mui-error': { color: red400 },
   '& .MuiOutlinedInput-root.Mui-error': {
-    '& fieldset': { 'border-color': red },
+    '& fieldset': { 'border-color': red400 },
   },
 
   // Error & Focus
-  '& label.Mui-error.Mui-focused': { color: red },
-  '& label span.Mui-error.Mui-focused': { color: red },
+  '& label.Mui-error.Mui-focused': { color: red400 },
+  '& label span.Mui-error.Mui-focused': { color: red400 },
   '& .MuiOutlinedInput-root.Mui-error.Mui-focused': {
-    '& fieldset': { 'border-color': red },
+    '& fieldset': { 'border-color': red400 },
   },
 
   // Focus
-  '& label.Mui-focused': { color: indigo },
-  '& label span.Mui-focused': { color: indigo },
+  '& label.Mui-focused': { color: indigo400 },
+  '& label span.Mui-focused': { color: indigo400 },
   '& .MuiOutlinedInput-root.Mui-focused': {
-    '& fieldset': { 'border-color': indigo },
+    '& fieldset': { 'border-color': indigo400 },
   },
 });
 
