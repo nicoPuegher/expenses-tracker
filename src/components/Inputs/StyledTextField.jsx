2c2ff25d653d703c8a65f7072fac92d31ece5cd1
Nico <nicopuegher@gmail.com>
2023-07-17 20:35:39 -0300

    format styled text field for better read

--- src/components/Inputs/StyledTextField.jsx
+++ src/components/Inputs/StyledTextField.jsx
@@ -1,19 +1,36 @@
 import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 
+const gray = '#6b7280'; // gray-500
+const slate = '#334155'; // slate-700
+const red = '#f87171'; // red-400
+const indigo = '#818cf8'; // indigo-400
+
 const StyledTextField = styled(TextField)({
-  '& label': { color: '#6b7280' }, // gray-500
-  '& label.Mui-error': { color: '#f87171' }, // red-400
-  '& label span.Mui-error': { color: '#f87171' }, // red-400
-  '& p.Mui-error': { color: '#f87171' }, // red-400
+  // Default
+  '& label': { color: gray },
+  '& div input': { color: slate },
+
+  // Error
+  '& label.Mui-error': { color: red },
+  '& label span.Mui-error': { color: red },
+  '& p.Mui-error': { color: red },
   '& .MuiOutlinedInput-root.Mui-error': {
-    '& fieldset': { 'border-color': '#f87171' }, // red-400
+    '& fieldset': { 'border-color': red },
   },
+
+  // Error & Focus
+  '& label.Mui-error.Mui-focused': { color: red },
+  '& label span.Mui-error.Mui-focused': { color: red },
   '& .MuiOutlinedInput-root.Mui-error.Mui-focused': {
-    '& fieldset': { 'border-color': '#f87171' }, // red-400
+    '& fieldset': { 'border-color': red },
   },
+
+  // Focus
+  '& label.Mui-focused': { color: indigo },
+  '& label span.Mui-focused': { color: indigo },
   '& .MuiOutlinedInput-root.Mui-focused': {
-    '& fieldset': { 'border-color': '#818cf8' }, // indigo-400
+    '& fieldset': { 'border-color': indigo },
   },
 });
 
