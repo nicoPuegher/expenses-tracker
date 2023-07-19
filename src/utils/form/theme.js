006c92497750f0ccc3d2395c90d1037d55a753ff
Nico <nicopuegher@gmail.com>
2023-07-19 14:45:37 -0300

    add a theme file with primary support and neutral color palettes

--- src/utils/form/theme.js
+++ src/utils/form/theme.js
@@ -1,11 +1,18 @@
 import { createTheme } from '@mui/material';
 
+const slate50 = '#f8fafc';
+const slate700 = '#334155';
+const slate400 = '#94a3b8';
+const red400 = '#f87171';
+const indigo400 = '#818cf8';
+
 const theme = createTheme({
   components: {
+    // Calendar container box
     MuiPickersLayout: {
       styleOverrides: {
         root: {
-          backgroundColor: 'green',
+          backgroundColor: slate50,
         },
       },
     },
