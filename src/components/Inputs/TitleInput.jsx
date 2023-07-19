a1361ebb9acd9b10590b9883ac70312a30823ebe
Nico <nicopuegher@gmail.com>
2023-07-19 15:55:06 -0300

    update title input import for styled text field

--- src/components/Inputs/TitleInput.jsx
+++ src/components/Inputs/TitleInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StyledTextField from './StyledTextField';
+import { StyledTextField } from './StyledInput';
 
 function TitleInput({ value, onChange, error, helperText }) {
   return (
