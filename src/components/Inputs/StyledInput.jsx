94949906690f769e22b231bd91dd914bbc137ef1
Nico <nicopuegher@gmail.com>
2023-07-19 16:01:41 -0300

    add styled input for text field and date picker with only default styles using new theme variables

---
+++ src/components/Inputs/StyledInput.jsx
@@ -0,0 +1,30 @@
+import TextField from '@mui/material/TextField';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { styled } from '@mui/material/styles';
+import { primary, neutral } from '../../utils/theme/theme';
+
+const sharedStyles = {
+  // Default
+  '& label': { color: neutral[400] },
+  '& label span': { color: neutral[400] },
+  '& div input': { color: neutral[600] },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': { borderColor: neutral[400] },
+  },
+  '& .MuiTypography-root': {
+    color: neutral[600],
+    lineHeight: 1,
+    marginTop: '2px',
+  },
+  '& .MuiSvgIcon-root': { color: neutral[400] },
+};
+
+const StyledTextField = styled(TextField)({
+  ...sharedStyles,
+});
+
+const StyledDatePicker = styled(DatePicker)({
+  ...sharedStyles,
+});
+
+export { StyledTextField, StyledDatePicker };
