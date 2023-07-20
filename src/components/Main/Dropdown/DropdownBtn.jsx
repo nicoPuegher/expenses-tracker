7a5a6511da22e193965bee434f29854515dc3100
Nico <nicopuegher@gmail.com>
2023-07-20 19:57:32 -0300

    remove unnecessary div in dropdown btn component

--- src/components/Main/Dropdown/DropdownBtn.jsx
+++ src/components/Main/Dropdown/DropdownBtn.jsx
@@ -5,15 +5,13 @@ import PropTypes from 'prop-types';
 
 function DropdownBtn({ currentFilter }) {
   return (
-    <div>
-      <Menu.Button className="inline-flex gap-x-1.5 rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-        {currentFilter}
-        <ChevronDownIcon
-          className="-mr-1 h-5 w-5 text-gray-400"
-          aria-hidden="true"
-        />
-      </Menu.Button>
-    </div>
+    <Menu.Button className="inline-flex gap-x-1.5 rounded bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+      {currentFilter}
+      <ChevronDownIcon
+        className="-mr-1 h-5 w-5 text-gray-400"
+        aria-hidden="true"
+      />
+    </Menu.Button>
   );
 }
 
