1a175b0bf5a3e8a3f3f9f1318ce77e50622f1e48
Nico <nicopuegher@gmail.com>
2023-07-20 19:32:34 -0300

    update button component to include ref and more common styles

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ className, type, onClick, txt }) {
-  return (
-    <button
-      className={`${className} rounded bg-primary-400 px-6 py-2 text-primary-50 outline-offset-4 hover:bg-primary-500 focus:outline-indigo-400`}
-      type={type ? 'submit' : 'button'}
-      onClick={onClick}
-    >
-      {txt}
-    </button>
-  );
-}
+const Button = React.forwardRef(
+  ({ className, type, onClick, txt, kind }, ref) => {
+    const btn =
+      kind === 'active' ? (
+        <button
+          className={`${className} inline-flex items-center justify-center rounded bg-primary-400 px-6 py-2 text-sm font-semibold text-primary-50 shadow-sm outline-offset-4 hover:bg-primary-500 focus:outline-indigo-400`}
+          type={type ? 'submit' : 'button'}
+          onClick={onClick}
+        >
+          {txt}
+        </button>
+      ) : (
+        <button
+          className={`${className} inline-flex items-center justify-center rounded bg-neutral-50 px-6 py-2 text-sm text-neutral-600 shadow-sm outline-offset-4 ring-1 ring-inset ring-slate-200 hover:bg-neutral-100 focus:outline-indigo-400`}
+          type={type ? 'submit' : 'button'}
+          onClick={onClick}
+          ref={ref}
+        >
+          {txt}
+        </button>
+      );
+
+    return btn;
+  }
+);
 
 Button.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
   txt: PropTypes.string.isRequired,
+  kind: PropTypes.string.isRequired,
 };
 
 Button.defaultProps = {
   className: '',
+  onClick: () => {},
 };
 
 export default Button;
