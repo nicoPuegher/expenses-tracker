b5ba7c510924871a786ac0b0d40452dc9a2cfe41
Nico <nicopuegher@gmail.com>
2023-07-20 15:58:08 -0300

    update button component to accept extra classes

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ type, onClick, txt }) {
+function Button({ className, type, onClick, txt }) {
   return (
     <button
-      className="rounded bg-primary-400 px-6 py-2 text-primary-50 outline-offset-4 hover:bg-primary-500 focus:outline-indigo-400"
+      className={`${className} rounded bg-primary-400 px-6 py-2 text-primary-50 outline-offset-4 hover:bg-primary-500 focus:outline-indigo-400`}
       type={type ? 'submit' : 'button'}
       onClick={onClick}
     >
@@ -14,9 +14,14 @@ function Button({ type, onClick, txt }) {
 }
 
 Button.propTypes = {
+  className: PropTypes.string,
   type: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   txt: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+  className: '',
 };
 
 export default Button;
