1682f8db40ddbd892640cea07a6892cc67cc7fad
Nico <nicopuegher@gmail.com>
2023-07-20 21:31:08 -0300

    update dropdown button component to use theme variables

--- src/components/Main/Dropdown/DropdownBtn.jsx
+++ src/components/Main/Dropdown/DropdownBtn.jsx
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 
 function DropdownBtn({ currentFilter }) {
   return (
-    <Menu.Button className="inline-flex gap-x-1.5 rounded bg-neutral-50 px-3 py-2 text-sm font-semibold text-neutral-600 shadow-sm outline-offset-4 ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100 focus:outline-primary-400">
+    <Menu.Button className="inline-flex gap-x-1.5 rounded bg-neutral-50 px-6 py-2 text-sm font-semibold text-neutral-600 shadow-sm outline-offset-4 ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100 focus:outline-primary-400">
       {currentFilter}
       <ChevronDownIcon
-        className="-mr-1 h-5 w-5 text-neutral-600"
+        className="-mr-2 h-5 w-5 text-neutral-600"
         aria-hidden="true"
       />
     </Menu.Button>
