1deaab1b33fcc8714db3909766062500d9bb39c6
Nico <nicopuegher@gmail.com>
2023-07-21 16:42:40 -0300

    add custom button in chart bar instead of default

--- src/components/Main/Chart/ChartBar.jsx
+++ src/components/Main/Chart/ChartBar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ExpensesContext from '../../../store/expenses-context';
 import barSize from '../../../utils/chart/bar-size';
+import Button from '../../UI/Button/Button';
 
 function ChartBar({ name, long, month, expenses, yearlyExpenses }) {
   const {
@@ -19,17 +20,33 @@ function ChartBar({ name, long, month, expenses, yearlyExpenses }) {
   const size = barSize(expenses, yearlyExpenses);
 
   return (
-    <button
-      type="button"
-      className={`${activeClass} mx-2 flex items-center justify-between`}
+    <Button
+      className=""
+      type=""
+      value={0}
       onClick={clickHandler}
+      txt=""
+      kind="inactive"
     >
       <div>{name}</div>
       <div className="flex h-3.5 w-2/3 items-center bg-slate-400">
         <div className="mx-0.5 h-2 bg-black" style={{ width: size }} />
       </div>
-    </button>
+    </Button>
   );
+
+  // return (
+  //   <button
+  //     type="button"
+  //     className={`${activeClass} mx-2 flex items-center justify-between`}
+  //     onClick={clickHandler}
+  //   >
+  //     <div>{name}</div>
+  //     <div className="flex h-3.5 w-2/3 items-center bg-slate-400">
+  //       <div className="mx-0.5 h-2 bg-black" style={{ width: size }} />
+  //     </div>
+  //   </button>
+  // );
 }
 
 ChartBar.propTypes = {
