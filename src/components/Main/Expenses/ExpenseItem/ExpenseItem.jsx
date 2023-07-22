f757066f225d431bcd1198623020fa2607301bab
Nico <nicopuegher@gmail.com>
2023-07-21 22:13:11 -0300

    update expense item component to use theme variables

--- src/components/Main/Expenses/ExpenseItem/ExpenseItem.jsx
+++ src/components/Main/Expenses/ExpenseItem/ExpenseItem.jsx
@@ -9,12 +9,12 @@ function ExpenseItem({ expData }) {
       <ExpenseDate date={expData.date} />
       <div className="flex flex-auto items-center justify-between">
         <div className="text-left">
-          <p className="pb-1.5 text-sm font-semibold leading-none text-gray-900">
+          <p className="pb-1.5 text-sm font-semibold leading-none text-neutral-600">
             {expData.title}
           </p>
-          <ExpenseTag name={expData.type} color="bg-gray-50" />
+          <ExpenseTag name={expData.type} />
         </div>
-        <p className="text-base text-gray-500">${expData.amount}</p>
+        <p className="text-base text-neutral-600">${expData.amount}</p>
       </div>
     </div>
   );
