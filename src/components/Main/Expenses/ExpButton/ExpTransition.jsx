93382cf5f6797d7f1dec8ff9ebaa32a4929927be
Nico <nicopuegher@gmail.com>
2023-07-21 22:14:50 -0300

    update exp transition component to use theme variables

--- src/components/Main/Expenses/ExpButton/ExpTransition.jsx
+++ src/components/Main/Expenses/ExpButton/ExpTransition.jsx
@@ -3,7 +3,6 @@ import { Menu, Transition } from '@headlessui/react';
 import PropTypes from 'prop-types';
 import Modal from '../../../Modal/Modal';
 import Form from '../../../Forms/Form';
-// import EditForm from '../../../Forms/EditForm';
 import ExpOptions from './ExpOptions';
 
 function ExpTransition({ expData }) {
@@ -26,7 +25,7 @@ function ExpTransition({ expData }) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 top-0 z-10 h-full w-full rounded bg-white px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Items className="absolute right-0 top-0 z-10 h-full w-full rounded px-3 py-2">
           <div className="w-full py-1">
             <ExpOptions expData={expData} onClick={openModalHandler} />
           </div>
