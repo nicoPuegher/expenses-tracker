ee5c2305f1684ec6704125ea515504edda72b817
Nico <nicopuegher@gmail.com>
2023-07-21 22:12:32 -0300

    update exp button component to use theme variables

--- src/components/Main/Expenses/ExpButton/ExpButton.jsx
+++ src/components/Main/Expenses/ExpButton/ExpButton.jsx
@@ -8,7 +8,7 @@ function ExpButton({ expData }) {
   return (
     <Menu as="div" className="relative">
       <div>
-        <Menu.Button className="w-full rounded bg-white px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Button className="w-full rounded bg-neutral-50 px-3 py-2 shadow-sm ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100">
           <ExpenseItem expData={expData} />
         </Menu.Button>
       </div>
