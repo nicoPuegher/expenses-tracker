004859ddf27acbd539e7c2340cee00f1f5ea5f7e
Nico <nicopuegher@gmail.com>
2023-07-23 15:07:46 -0300

    update exp buttonn component classes

--- src/components/Main/Expenses/ExpButton/ExpButton.jsx
+++ src/components/Main/Expenses/ExpButton/ExpButton.jsx
@@ -7,11 +7,9 @@ import ExpTransition from './ExpTransition';
 function ExpButton({ expData }) {
   return (
     <Menu as="div" className="relative">
-      <div>
-        <Menu.Button className="w-full rounded bg-neutral-50 px-3 py-2 shadow-sm ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100">
-          <ExpenseItem expData={expData} />
-        </Menu.Button>
-      </div>
+      <Menu.Button className="w-full rounded bg-neutral-50 px-5 py-2.5 shadow-sm shadow-neutral-900 ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-primary-400 lg:px-6">
+        <ExpenseItem expData={expData} />
+      </Menu.Button>
       <ExpTransition expData={expData} />
     </Menu>
   );
