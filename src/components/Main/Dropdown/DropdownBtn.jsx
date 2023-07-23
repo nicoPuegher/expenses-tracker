6faef7a905c278be4bf074aee9d3abceca377efe
Nico <nicopuegher@gmail.com>
2023-07-23 15:03:18 -0300

    update dropdown btn component classes

--- src/components/Main/Dropdown/DropdownBtn.jsx
+++ src/components/Main/Dropdown/DropdownBtn.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 function DropdownBtn({ currentFilter }) {
   return (
-    <Menu.Button className="inline-flex items-center gap-x-0.5 rounded bg-neutral-50 px-5 py-2.5 font-semibold text-neutral-600 shadow-sm shadow-neutral-900 ring-1 ring-inset ring-neutral-200 hover:bg-neutral-200 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-400">
+    <Menu.Button className="inline-flex items-center gap-x-0.5 rounded bg-neutral-50 px-5 py-2.5 text-sm font-semibold text-neutral-600 shadow-sm shadow-neutral-900 ring-1 ring-inset ring-neutral-200 hover:bg-neutral-100 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-400 lg:px-6">
       {currentFilter}
       <ChevronDownIcon
         className="-mr-2 h-5 w-5 text-neutral-600"
