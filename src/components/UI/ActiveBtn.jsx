be73699b51b957b25d4a59a7143fcc24ecc5770e
Nico <nicopuegher@gmail.com>
2023-07-23 14:58:46 -0300

    update active btn component classes

--- src/components/UI/ActiveBtn.jsx
+++ src/components/UI/ActiveBtn.jsx
@@ -6,7 +6,7 @@ function ActiveBtn({ className, type, onClick, txt }) {
   return (
     <button
       className={twMerge(
-        'rounded bg-primary-400 px-5 py-2.5 font-semibold text-primary-50 shadow-sm shadow-neutral-300 hover:bg-primary-500 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-400 lg:px-6',
+        'rounded bg-primary-400 px-5 py-2.5 text-sm font-semibold text-primary-50 shadow-sm shadow-neutral-300 hover:bg-primary-500 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-400 lg:px-6',
         className
       )}
       type={type === 'submit' ? 'submit' : 'button'}
