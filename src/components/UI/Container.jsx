dd91d4039163a090bfc2794d10d1d6fc57a41ae1
Nico <nicopuegher@gmail.com>
2023-07-22 21:14:57 -0300

    update container component classes

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -4,7 +4,9 @@ import { twMerge } from 'tailwind-merge';
 
 function Container({ className, children }) {
   return (
-    <div className={twMerge('rounded-md py-3', className)}>{children}</div>
+    <div className={twMerge('rounded-md p-3 md:px-4 lg:px-5', className)}>
+      {children}
+    </div>
   );
 }
 
