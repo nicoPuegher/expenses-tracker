a15c923dd9b63903dd8a30f38c0458426ee283a2
Nico <nicopuegher@gmail.com>
2023-07-23 22:33:26 -0300

    update chart component classes

--- src/components/Main/Chart/Chart.jsx
+++ src/components/Main/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import ChartBars from './ChartBars';
 
 function Chart() {
   return (
-    <div className="mb-3 rounded-md bg-neutral-50 py-3 md:mb-5">
+    <div className="mb-3 rounded-md bg-neutral-50 py-3 md:mb-4 2xl:mb-5">
       <div className="grid grid-cols-2 lg:h-28 lg:grid-cols-12 lg:gap-1 lg:px-0.5">
         <ChartBars />
       </div>
