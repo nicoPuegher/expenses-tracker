0e5c8ae8e45877d9e0ca7c25740c15aad0dad599
Nico <nicopuegher@gmail.com>
2023-07-23 22:32:45 -0300

    update header component classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import New from './New';
 
 function Header() {
   return (
-    <Container className="mb-3 flex w-full items-center gap-6 p-0 md:mb-5 md:px-0 lg:p-0">
+    <Container className="mb-3 flex w-full items-center gap-6 p-0 md:mb-4 md:p-0 lg:p-0 2xl:mb-5">
       <Name />
       <New />
     </Container>
